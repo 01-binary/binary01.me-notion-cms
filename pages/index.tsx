105caf801a426b1768d0476714406c781b6a3d43
jinsoo <admin@binary01.me>
2023-06-05 21:58:21 +0900

    feat: connect notion db

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,23 @@
+import React from 'react';
+
+import { GetStaticProps } from 'next';
+
+import { getDatabaseItems } from '@/utils/notionClient';
+
 const Home = () => {
-  return <div className="font-bold">Hello</div>;
+  return <div>Home</div>;
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  if (!process.env.DATABASE_ID) throw new Error('DATABASE_ID is not defined');
+
+  const databaseItems = await getDatabaseItems(process.env.DATABASE_ID);
+
+  console.log('databaseItems :>> ', databaseItems);
+
+  return {
+    props: {},
+  };
+};
