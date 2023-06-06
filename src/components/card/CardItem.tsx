6544da99c9d4df629acb05cc5b8cfb7331f9cda1
jinsoo <admin@binary01.me>
2023-06-06 17:06:00 +0900

    fix: a tag delete

--- src/components/card/CardItem.tsx
+++ src/components/card/CardItem.tsx
@@ -17,25 +17,23 @@ const CardItem = ({ cardItem }: CardItemProps) => {
   return (
     <li className="group overflow-hidden rounded-2xl shadow-lg">
       <Link href={`blog/${id}`}>
-        <a>
-          <div className="relative aspect-[1.3/1]">
-            <Image
-              src={cover}
-              alt={title}
-              fill
-              className="transition-transform group-hover:scale-105"
-            />
-          </div>
+        <div className="relative aspect-[1.3/1]">
+          <Image
+            src={cover}
+            alt={title}
+            fill
+            className="transition-transform group-hover:scale-105"
+          />
+        </div>
 
-          <div className="flex flex-col gap-5 p-6">
-            <h4 className="flex flex-row items-center gap-1 text-2xl font-bold transition-colors group-hover:text-blue-500">
-              <IconRenderer icon={icon} />
-              {title}
-            </h4>
-            {description ? <p className="font-medium text-gray-600">{description}</p> : null}
-            <time className="font-medium text-gray-700">{published}</time>
-          </div>
-        </a>
+        <div className="flex flex-col gap-5 p-6">
+          <h4 className="flex flex-row items-center gap-1 text-2xl font-bold transition-colors group-hover:text-blue-500">
+            <IconRenderer icon={icon} />
+            {title}
+          </h4>
+          {description ? <p className="font-medium text-gray-600">{description}</p> : null}
+          <time className="font-medium text-gray-700">{published}</time>
+        </div>
       </Link>
       {/* tags */}
     </li>
