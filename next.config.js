70c26b5fb6f22e6e716a974cad8bd36b588f498a
jinsoo <binary01@kakao.com>
2023-07-06 14:35:25 +0900

    feat: image unoptimized add

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    unoptimized: true,
     domains: ['www.notion.so', 's3.us-west-2.amazonaws.com', 'images.unsplash.com'],
   },
 };
