5de7382ddf5791151d0ac6a4d14cba051fb632bf
jinsoo <binary01@kakao.com>
2023-07-07 20:04:58 +0900

    fix: cache control 삭제

--- next.config.js
+++ next.config.js
@@ -6,20 +6,6 @@ const nextConfig = {
     unoptimized: true,
     domains: ['www.notion.so', 's3.us-west-2.amazonaws.com', 'images.unsplash.com'],
   },
-  async headers() {
-    return [
-      {
-        // 전체 페이지에 대한 캐시 제어 헤더를 설정합니다.
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'max-age=0, s-maxage=60',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
