c48df27c539ae401d87832151307debb577166d7
jinsoo <binary01@kakao.com>
2023-08-12 19:49:26 +0900

    feat: add get year

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,10 +1,13 @@
+import dayjs from 'dayjs';
+
 import { siteConfig } from 'site.config';
 
 const Footer = () => {
+  const currentYear = dayjs().year();
   return (
     <footer className="mx-auto mt-[70px] mb-6 max-w-[1024px]">
       <section className="text-center text-[14px] text-[#37352F]">
-        Copyright 2023 {siteConfig.author}
+        Copyright {currentYear} {siteConfig.author}
       </section>
     </footer>
   );
