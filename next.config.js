d7d6f757dc2b03a1b813ab7f5d40c397f1965d87
jinsoo <binary01@kakao.com>
2024-05-04 21:06:10 +0900

    feat: next  scrollRestoration add

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     unoptimized: true,
     domains: ['www.notion.so', 's3.us-west-2.amazonaws.com', 'images.unsplash.com'],
   },
+  experimental: {
+    scrollRestoration: true,
+  },
 };
 
 module.exports = nextConfig;
