ab0a6316f5a59b040fd78da40f0156cb5d565a09
jinsoo <binary01@kakao.com>
2024-10-10 15:03:53 +0900

    feat: react-notion css embedding

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,10 @@ import type { AppProps } from 'next/app';
 
 import Layout from '@/components/layout';
 
-import 'react-notion-x/src/styles.css';
 import 'prismjs/themes/prism-tomorrow.css';
 import 'katex/dist/katex.min.css';
 import '@/assets/styles/index.css';
+import '@/assets/styles/notion-x.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
