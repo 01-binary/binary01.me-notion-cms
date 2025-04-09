adbacea7b8154a37cde1bf01c0a3b4505a4bfc6a
jinsoo <binary01@kakao.com>
2025-04-10 01:06:01 +0900

    feat: Replace react-notion-x with notion-to-jsx for rendering Notion content

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,43 +1,42 @@
 import { ParsedUrlQuery } from 'querystring';
 
 import { GetStaticPaths, GetStaticProps } from 'next';
-import { NotionRenderer } from 'react-notion-x';
+import { NotionBlock, Renderer } from 'notion-to-jsx';
 
-import PostRenderer from '@/features/posts/Renderer';
-import { ExtendedRecordMap } from '@/interfaces/notion';
-import { getIdBySlug, getNotionPosts, getPage, getPageProperties, getSlugs } from '@/utils';
-import { getPreviewImageFromRecordMap } from '@/utils';
-import { siteConfig } from 'site.config';
+import { getIdBySlug, getNotionPosts, getSlugs, notionClient } from '@/utils';
 
 import Giscus from '@/components/common/Giscus';
 import PageHead from '@/components/common/PageHead';
 
 import { REVALIDATE_TIME } from '@/assets/constants';
 
-import { getPageTitle } from '@/utils/getPageTitle';
-
+import 'notion-to-jsx/dist/index.css';
 interface Props {
-  recordMap: ExtendedRecordMap;
+  blocks: NotionBlock[];
   seo: {
     title: string;
     description: string;
     keywords: string;
-    ogImage: string | null;
+    coverUrl: string;
   };
 }
 
-const PostPage = ({ recordMap, seo: { title, description, keywords, ogImage } }: Props) => {
-  if (!recordMap) return null;
+const PostPage = ({ blocks, seo: { title, description, keywords, coverUrl } }: Props) => {
+  if (!blocks) return null;
   return (
     <>
       <PageHead
         title={title}
         description={description}
         keywords={keywords}
-        image={ogImage}
+        image={coverUrl}
       />
       <article>
-        <PostRenderer recordMap={recordMap as Parameters<typeof NotionRenderer>[0]['recordMap']} />
+        <Renderer
+          title={title}
+          cover={coverUrl}
+          blocks={blocks}
+        />
       </article>
       <div className="mx-auto max-w-[900px] px-4">
         <Giscus />
@@ -55,24 +54,17 @@ interface PostParams extends ParsedUrlQuery {
 export const getStaticProps: GetStaticProps<Props, PostParams> = async ({ params }) => {
   const { slug } = params as PostParams;
   const id = await getIdBySlug(slug as string, process.env.NOTION_POST_DATABASE_ID as string);
-  const recordMap = await getPage(id);
-  const { description, keywords } = await getPageProperties(id);
-  const previewImages = await getPreviewImageFromRecordMap(recordMap);
-
-  const title = getPageTitle(recordMap) as string;
-  const ogImage = `${siteConfig.url}/api/cover-image?pageId=${id}`;
+  const blocks = (await notionClient.getPageBlocks(id)) as NotionBlock[];
+  const properties = await notionClient.getPageProperties(id);
 
   return {
     props: {
-      recordMap: {
-        ...recordMap,
-        preview_images: previewImages,
-      } as ExtendedRecordMap,
+      blocks,
       seo: {
-        title,
-        description,
-        keywords,
-        ogImage,
+        title: properties?.['Name'] || '',
+        description: properties?.['Desc'] || '',
+        keywords: properties?.['Category']?.['name'] || '',
+        coverUrl: properties?.['coverUrl'] || '',
       },
     },
     revalidate: REVALIDATE_TIME,
