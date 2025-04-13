ed8e5ec3e42566c410d54fcc677df73282a41209
jinsoo <binary01@kakao.com>
2025-04-13 19:18:41 +0900

    fix: css import order 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,8 @@ import Layout from '@/components/layout';
 
 import 'prismjs/themes/prism-tomorrow.css';
 
-import '@/assets/styles/index.css';
 import 'notion-to-jsx/dist/index.css';
+import '@/assets/styles/index.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
