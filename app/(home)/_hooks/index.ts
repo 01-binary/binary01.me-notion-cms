1723c21991d0202ad0400381e448f44976b7f7cf
jinsoo <binary01@kakao.com>
2026-01-29 23:29:19 +0900

    refactor: 무한 스크롤 훅 구조 단순화

--- app/(home)/_hooks/index.ts
+++ app/(home)/_hooks/index.ts
@@ -1,3 +1,3 @@
 export { default as useCategoryQueryParam } from './useCategoryQueryParam';
-export { default as useFilteredPosts } from './useFilteredPosts';
+export { default as useFilteredPagedPosts } from './useFilteredPagedPosts';
 export { default as useTiltEffect } from './useTiltEffect';
